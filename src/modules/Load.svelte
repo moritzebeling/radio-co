a2bb4fb4de11ae5b4d15cb158c5c990f2f2f120b
moritzebeling <git@moritzebeling.com>
2021-03-22 18:29:39 +0100

    refactor nowplaying module

---
+++ src/modules/Load.svelte
@@ -0,0 +1,18 @@
+<script>
+
+    import { load } from '../utilities/load.js';
+    export let url;
+
+    let data = load(url);
+
+</script>
+
+{#await data}
+    <p>Loading</p>
+{:then data}
+
+    <slot prop={data}></slot>
+
+{:catch error}
+    <p class="error">Error</p>
+{/await}
--- src/modules/NowPlaying.svelte
+++ src/modules/NowPlaying.svelte
@@ -1,28 +1,12 @@
 <script>
 
-    import { load } from '../utilities/load.js';
-    let data = load('http://stern.rocks/api/nowplaying');
+    import Load from './Load.svelte';
+    const url = 'http://stern.rocks/api/nowplaying';
 
 </script>
 
-{#await data}
-    <p>Loading</p>
-{:then data}
+<Load {url} let:prop={data}>
 
     <pre>{JSON.stringify( data, null, 2 )}</pre>
 
-{:catch error}
-    <p class="error">Error</p>
-{/await}
-
-<style>
-
-    pre {
-        background-color: #eee;
-        margin: 0.5rem;
-        padding: 0.5rem;
-        font-family: monospace;
-        border-radius: 1rem;
-    }
-
-</style>
+</Load>
